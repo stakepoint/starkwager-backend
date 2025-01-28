31b46c3985c9549caf14b5a8a5a8a135501494ee
xaxxoo <bnahmad83@gmail.com>
2025-01-29 00:23:14 +0100

    feat: add user registration

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,11 +2,24 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+
+  
+  ) { }
+  
+  @Post('register')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
 
   @Post('create-login')
   create(@Body() createAuthDto: CreateAuthDto) {
