7863ed60ce81cd8f649a16c68231e56fdd74bd01
olumidehub <olumide@circo.live>
2025-02-21 13:36:34 +0100

    feat: add_hashtag_to_wager

--- backend/src/wager/services/wager.service.ts
+++ backend/src/wager/services/wager.service.ts
@@ -10,8 +10,43 @@ export class WagerService {
     return await this.prisma.wager.create({ data });
   }
 
-  async getAllWagers(status: WagerStatus) {
-    return await this.prisma.wager.findMany({ where: { status } });
+  async getAllWagers(
+    status: WagerStatus,
+    hashtags?: string,
+    filterType: 'AND' | 'OR' = 'OR',
+  ) {
+    const hashtagList = hashtags ? hashtags.split(',') : [];
+
+    const query: any = {
+      where: {
+        status,
+      },
+      include: {
+        hashtags: true,
+      },
+    };
+
+    if (hashtagList.length > 0) {
+      if (filterType === 'AND') {
+        query.where.hashtags = {
+          every: {
+            name: {
+              in: hashtagList,
+            },
+          },
+        };
+      } else {
+        query.where.hashtags = {
+          some: {
+            name: {
+              in: hashtagList,
+            },
+          },
+        };
+      }
+    }
+
+    return await this.prisma.wager.findMany(query);
   }
 
   async findOneById(id: string) {
